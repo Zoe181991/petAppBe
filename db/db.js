0f18c19f7a14934d1b182230a6e8fb398cc91c25
Zoe181991 <zoebarkan@gmail.com>
2024-04-01 19:04:27 +0300

    ..

--- db/db.js
+++ db/db.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 main().catch((err) => console.log(err));
 async function main() {
-  await mongoose.connect(`${process.env.MONGO_URI}`, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  });
+  await mongoose.connect(
+    "mongodb+srv://admin:admin181991@clusterpet.yp3c5w9.mongodb.net/PetsApp",
+    { useUnifiedTopology: true, useNewUrlParser: true }
+  );
   console.log("Mongoose is connected");
 }
 
